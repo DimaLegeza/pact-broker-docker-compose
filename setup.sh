97529fe1e0b1e31340767eb89849e87f1077af66
Dima Legeza <dmytro.legeza@gmail.com>
2020-11-19 15:46:32 +0100

    Add extra verifications

--- setup.sh
+++ setup.sh
@@ -1,7 +1,19 @@
 #!/bin/bash
 
-sudo apt update
-sudo apt --assume-yes install docker.io
-sudo apt --assume-yes install docker-compose
+if ! [ -x "$(command -v docker)" ]; then
+	sudo apt update
+	sudo apt --assume-yes install docker.io
+	sudo apt --assume-yes install docker-compose
+	sudo usermod -aG docker ${USER}
+	logout
+fi
+if [[ -z "$PACT_BROKER_BASIC_AUTH_USERNAME" ]]
+then
+	echo "Please set \$PACT_BROKER_BASIC_AUTH_USERNAME"
+fi
+if [[ -z "$PACT_BROKER_BASIC_AUTH_PASSWORD" ]]
+then
+        echo "Please set \$PACT_BROKER_BASIC_AUTH_PASSWORD"
+fi
 
 docker-compose up -d postgresql && sleep 5 && docker-compose up -d pact-broker
